7f1f17c5690d1009a4b3835e9e2e0c8bdfc2ab0c
lendlord <lendlord07@gmail.com>
2017-12-12 22:43:18 +0300

    hotlog position

--- wp-content/themes/Memoir/footer.php
+++ wp-content/themes/Memoir/footer.php
@@ -1,23 +1,21 @@
 		</div> <!-- end #content -->
 		<div id="content-bottom">
-            <div class="hotlog">
-                <!-- HotLog -->
-                <span id="hotlog_counter"></span>
-                <span id="hotlog_dyn"></span>
-                <script type="text/javascript">
-                    var hot_s = document.createElement('script');
-                    hot_s.type = 'text/javascript'; hot_s.async = true;
-                    hot_s.src = 'http://js.hotlog.ru/dcounter/2333865.js';
-                    hot_d = document.getElementById('hotlog_dyn');
-                    hot_d.appendChild(hot_s);
-                </script>
-                <noscript>
-                    <a href="http://click.hotlog.ru/?2333865" target="_blank"><img
-                                src="http://hit37.hotlog.ru/cgi-bin/hotlog/count?s=2333865&amp;im=80" border="0"
-                                alt="HotLog"></a>
-                </noscript>
-                <!-- /HotLog -->
-            </div>
+            <!-- HotLog -->
+            <span id="hotlog_counter"></span>
+            <span id="hotlog_dyn"></span>
+            <script type="text/javascript">
+                var hot_s = document.createElement('script');
+                hot_s.type = 'text/javascript'; hot_s.async = true;
+                hot_s.src = 'http://js.hotlog.ru/dcounter/2333865.js';
+                hot_d = document.getElementById('hotlog_dyn');
+                hot_d.appendChild(hot_s);
+            </script>
+            <noscript>
+                <a href="http://click.hotlog.ru/?2333865" target="_blank"><img
+                            src="http://hit37.hotlog.ru/cgi-bin/hotlog/count?s=2333865&amp;im=80" border="0"
+                            alt="HotLog"></a>
+            </noscript>
+            <!-- /HotLog -->
             <p id="copyright">
                 Copyright 2017 Фаэтон
             </p>
